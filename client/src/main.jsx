5b1d6226033aa154b338d54f0d2045e641a87f7b
Mons <46229551+Monsc@users.noreply.github.com>
2025-05-31 11:17:46 +0200

    fix: 全局Provider包裹App，修复useContext空白页问题

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { AuthProvider } from './contexts/AuthContext';
+import { ToastProvider } from './contexts/ToastContext';
 
 console.log('API_BASE_URL from main:', import.meta.env.VITE_API_BASE);
 
@@ -9,6 +11,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <ToastProvider>
+        <App />
+      </ToastProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
